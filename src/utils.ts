090f970b700d08ac514f17f7ee2edfdbed194111
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-04-02 11:45:21 +0200

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,8 @@
 export function retry<T>(fn: () => Promise<T>, retries: number): Promise<T> {
   return fn().catch((err) => {
+    console.log(`Remaining retries ${retries}`);
+    console.log("Current error:");
+    console.log(err);
     if (retries <= 0) {
       throw err;
     }
