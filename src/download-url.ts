63edbc23b5c134a8f49d218a07325331a22cb2f6
Jozef Steinhübl <contact@xhyrom.dev>
2026-01-04 23:27:55 +0100

    fix: drop sha support for now

--- src/download-url.ts
+++ src/download-url.ts
@@ -15,78 +15,12 @@ export async function getDownloadMeta(options: Input): Promise<DownloadMeta> {
     };
   }
 
-  if (options.version && /^[0-9a-f]{40}$/i.test(options.version)) {
-    return await getShaDownloadMeta(options);
-  }
-
   return await getSemverDownloadMeta(options);
 }
 
 interface Run {
   id: string;
   head_sha: string;
-}
-
-interface Runs {
-  workflow_runs: Run[];
-}
-
-async function getShaDownloadMeta(options: Input): Promise<DownloadMeta> {
-  let res: Runs;
-  let page = 1;
-  let run: Run | undefined;
-  while (
-    (res = (await (
-      await request(
-        `https://api.github.com/repos/oven-sh/bun/actions/workflows/ci.yml/runs?per_page=100&page=${page}`,
-        {
-          headers: options.token
-            ? { "Authorization": `Bearer ${options.token}` }
-            : {},
-        },
-      )
-    ).json()) as Runs)
-  ) {
-    if (res.workflow_runs.length === 0) {
-      break;
-    }
-
-    run = res.workflow_runs.find((item) => item.head_sha === options.version);
-    if (run) break;
-
-    page++;
-  }
-
-  if (!run) {
-    throw new Error(`Failed to find workflow run for SHA '${options.version}'`);
-  }
-
-  const artifacts = (await (
-    await request(
-      `https://api.github.com/repos/oven-sh/bun/actions/runs/${run.id}/artifacts`,
-      {
-        headers: options.token
-          ? { "Authorization": `Bearer ${options.token}` }
-          : {},
-      },
-    )
-  ).json()) as { artifacts: { name: string; archive_download_url: string }[] };
-
-  const { os, arch, avx2, profile, token } = options;
-
-  const name = `bun-${os ?? getPlatform()}-${arch ?? getArchitecture()}${
-    avx2 === false ? "-baseline" : ""
-  }${profile ? "-profile" : ""}`;
-
-  const artifact = artifacts.artifacts.find((item) => item.name === name);
-  if (!artifact) {
-    throw new Error(`Failed to find artifact '${name}' in run '${run.id}'`);
-  }
-
-  return {
-    url: artifact.archive_download_url,
-    auth: token ? `Bearer ${token}` : undefined,
-  };
 }
 
 async function getSemverDownloadMeta(options: Input): Promise<DownloadMeta> {
