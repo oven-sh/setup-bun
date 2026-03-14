82fc4cb2e0a09faa4d5c9718e5b5b16473bf63ce
tcely <tcely@users.noreply.github.com>
2026-03-14 19:50:19 -0400

    Tests for response-storage.ts

--- tests/bundled.suite.ts
+++ tests/bundled.suite.ts
@@ -3,7 +3,9 @@ import "./init.suite";
 
 import { register as registerAtomicTests } from "./atomic-write.test";
 import { register as registerFilesystemTests } from "./filesystem-cache.test";
+import { register as registerResponseTests } from "./response-storage.test";
 
 // Register all sub-suites
 registerAtomicTests();
 registerFilesystemTests();
+registerResponseTests();
